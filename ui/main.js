01c9bde22dc90e3a4a520f204d1df49acf22c18d
vbalakrishna <vuyyurubalakrishna@gmail.com>
2017-08-15 13:59:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,15 @@ console.log('Loaded!');
 var element=document.getElementById('mjs');
 
 element.innerHTML = 'New Value';
+
+var ele=document.getElementById('image');
+var left=0;
+
+function right(){
+    left=left+1;
+    ele.style.left=left+'px';
+}
+
+ele.onclick=function(){
+    setInterval(right,50);
+};
