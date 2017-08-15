844cd41b1eadf5565998adf38ec72e0290b4b102
vbalakrishna <vuyyurubalakrishna@gmail.com>
2017-08-15 16:39:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,18 +37,31 @@ button.onclick=function(){
     
 };
 
+ 
 
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('sbt_btn'); 
 submit.onclick=function(){
-    var names=['name1', 'name2', 'name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+     
+  var request=new XMLHttpRequest();
+  
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+                var names=['name1', 'name2', 'name3','name4'];
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+          }
+      }
+  };
+  request.open('GET','http://vuyyurubalakrishna.imad.hasura-app.io/submit-name?name='+name ,true);
+  request.send(null);
+  
 };
 
 
