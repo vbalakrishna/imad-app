828b0afe8f90f797de3a34cd27fa359191cd5a89
vbalakrishna <vuyyurubalakrishna@gmail.com>
2017-08-15 10:30:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,85 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articles={
+  articleOne:{
+      title:'Article One I BalaKrishna',
+      heading:'Article One',
+      date:'Aug,14 2017',
+      content:`
+                <p>
+                    This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.
+                </p>
+                <p>
+                    This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.
+                </p>
+                <p>
+                    This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.
+                </p>
+        `
+      
+  },
+  articleTwo:{
+      title:'Article Two I BalaKrishna',
+      heading:'Article Two',
+      date:'Aug,14 2017',
+      content:`
+                <p>
+                    This is my second article.
+                </p>
+        `
+  },
+  articleThree:{
+      title:'Article Three I BalaKrishna',
+      heading:'Article Three',
+      date:'Aug,14 2017',
+      content:`
+                <p>
+                    This is my third article.
+                </p>
+        `
+  }
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`
+        <html>
+            <head>
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                       ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        <p>
+                            ${content}
+                        </p>
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
+}
+
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
